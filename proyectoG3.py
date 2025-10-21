017d64ba96a67dddedd6262baa19f8e61e53186d
StibiCF <tu_correo@ejemplo.com>
2025-10-20 21:36:40 -0600

    modified:   proyectoIPC.py

--- proyectoG3.py
+++ proyectoG3.py
@@ -1,1 +1,5 @@
 #Este es el proyecto del grupo 3 ajflkasljasñlfjasdflkasdfjla
+#Mil años de seriedad
+print ("Mil años de seriedad")
+print ("Segunda línea de código añadida por el grupo 3")
+print ("Tercera línea de código añadida por el grupo 3")
