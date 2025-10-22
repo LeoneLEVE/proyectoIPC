784dda4c9bfa2a08eb63cab55ec6b4bd5a466daa
StibiCF <tu_correo@ejemplo.com>
2025-10-21 21:44:19 -0600

    modified:   proyectoG3.py

--- proyectoG3.py
+++ proyectoG3.py
@@ -1,5 +1,19 @@
-#Este es el proyecto del grupo 3 ajflkasljasñlfjasdflkasdfjla
-#Mil años de seriedad
-print ("Mil años de seriedad")
-print ("Segunda línea de código añadida por el grupo 3")
-print ("Tercera línea de código añadida por el grupo 3")
+def ROT13toBit(input_texto):
+    # Aplicamos cifrado ROT13
+    rot13_texto = ''
+    for char in input_texto:
+        if 'A' <= char <= 'Z':
+            rot13_texto += chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
+        elif 'a' <= char <= 'z':
+            rot13_texto += chr((ord(char) - ord('a') + 13) % 26 + ord('a'))
+        else:
+            rot13_texto += char  # No modifica espacios ni signos
+
+    # Convertir a cadenas de bits
+    bit_output = ' '.join(format(ord(c), '08b') for c in rot13_texto)
+    return bit_output
+
+#Cadena de texto desde la terminal
+mensaje = input("Escribe el mensaje que quieres cifrar: ")
+cifrado = ROT13toBit(mensaje)
+print("Mensaje cifrado en bits:", cifrado)
