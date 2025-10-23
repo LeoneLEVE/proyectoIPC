adeb84e4690a0b1faac24b8edffb30757036b4a9
CharlieDGM <samuelgozalesmaldonado@gmail.com>
2025-10-22 18:29:25 -0600

    Interfaz con cambios hechos

--- proyectoG3.py
+++ proyectoG3.py
@@ -1,44 +1,18 @@
 import tkinter as tk
 
 #####################################
-
+#FUNCIONES para convertir el string encriptado con ROT13 a binario y viceversa
 def ROT13toBit(texto_de_entrada):
-    rot13_texto = ''
-    for char in texto_de_entrada:
-        if 'A' <= char <= 'Z':
-            rot13_texto += chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
-        elif 'a' <= char <= 'z':
-            rot13_texto += chr((ord(char) - ord('a') + 13) % 26 + ord('a'))
-        else:
-            rot13_texto += char
-    bit_de_salida = ' '.join(format(ord(c), '08b') for c in rot13_texto)
+    bit_de_salida = ' '.join(format(ord(c), '08b') for c in texto_de_entrada)
     return bit_de_salida
 
 def BitToROT13(bit_de_entrada):
     chars = bit_de_entrada.split()
-    text = ''.join(chr(int(b, 2)) for b in chars)
-    texto_original = ''
-    for char in text:
-        if 'A' <= char <= 'Z':
-            texto_original += chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
-        elif 'a' <= char <= 'z':
-            texto_original += chr((ord(char) - ord('a') + 13) % 26 + ord('a'))
-        else:
-            texto_original += char
+    texto_original = ''.join(chr(int(b, 2)) for b in chars)
     return texto_original
 
-#Entrada desde terminal
-mensaje = input("Escribe el mensaje que quieres cifrar y luego recuperar: ")
-
-#Cifrado ROT13 a binario
-cifrado = ROT13toBit(mensaje)
-print("El mensaje cifrado en bits es:", cifrado)
-
-#Desencriptado desde bits a texto original
-descifrado = BitToROT13(cifrado)
-print("EL mensaje original recuperado es:", descifrado)
-
-#####################
+####################################
+#FUNCIONES para encriptar el string con ROT13
 def StringToROT13(texto):
     resultado = ""
     for c in texto:
@@ -49,30 +23,41 @@ def StringToROT13(texto):
         else:
             resultado += c
     return resultado
+
 def ROT13ToString(texto):
     return StringToROT13(texto)
-
+######################################
+#FUNCIONES GUI
 def mostrarBits(cadenaBits, contenedor, fila, columna):
-    anchoCuadro = 30
-    altoCuadro = 30
+    anchoCuadro = 15
+    altoCuadro = 15
     anchoTotal = len(cadenaBits) * anchoCuadro
 
     canva = tk.Canvas(contenedor, width=anchoTotal, height=altoCuadro)
-    canva.grid(row=fila, column=columna, columnspan=2, pady=10, padx=10)
+    canva.grid(row=fila, column=columna, pady=10, padx=10)
 
     for i, bit in enumerate(cadenaBits):
         coordenadaInicial = i * anchoCuadro
         coordenadaFinal = coordenadaInicial + anchoCuadro
-        color = "black" if bit == "0" else "white"
-        canva.create_rectangle(coordenadaFinal, 0, coordenadaFinal, altoCuadro, fill=color, outline="gray")
+        color = "black" if bit == "1" else ("white" if bit == "0" else "gray")
+        canva.create_rectangle(coordenadaInicial, 0, coordenadaFinal, altoCuadro, fill=color, outline="gray")
     #esto crea un rectangulo con "i" cantidad de cuadros, donde cada cuadro esta formado por las puntos (coordenadaFinal, 0) y (coordeanadaFinal, altoCuadro)
 
 def crearVentanaVisualizacion():
     segundaVentana = tk.Toplevel(menu)
     segundaVentana.title("Proceso de encriptación paso a paso")
+
+    strMensaje = inputMensaje.get()
+    tk.Label(segundaVentana, text="Mensaje encriptado con ROT13: ").grid(row=0, column=0, pady=10, padx=10)
+    tk.Label(segundaVentana, text=StringToROT13(strMensaje)).grid(row=0, column=1, padx=10, pady=10)
+
+    strMensaje = StringToROT13(strMensaje)
+    tk.Label(segundaVentana, text="Mensaje encriptado a binario: ").grid(row=1, column=0, padx=10, pady=10)
+    strMensaje = ROT13toBit(strMensaje)
+    mostrarBits(strMensaje, segundaVentana, 2, 2)
     
     botonContinuar = tk.Button(segundaVentana, text="Ver mensaje resultante", command=lambda:[segundaVentana.destroy(), crearVentanaResultado("pepe")])
-    botonContinuar.grid(row=1, columnspan=2, pady=10, padx=10)
+    botonContinuar.grid(columnspan=2, pady=10, padx=10)
 
 def crearVentanaResultado(mensajeDesencriptado):
     terceraVentana = tk.Toplevel(menu)
@@ -99,5 +84,3 @@ boton.grid(row=2, pady=10, columnspan=2)
 
 
 menu.mainloop()
-
-
